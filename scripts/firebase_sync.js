0452240e728c686b8ca261afbecb44e08e35fed0
datdn4 <datdn4@fsoft.com.vn>
2019-04-09 14:12:35 +0700

    Refactor code

--- scripts/firebase_sync.js
+++ scripts/firebase_sync.js
@@ -1,42 +1,53 @@
+/// MARK: Get bookmark tree
 function getBookmarkTree() {
-    var bookmarkTreeNodes = chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      	syncBookmarks(bookmarkTreeNodes);
-    });
+	chrome.bookmarks.getTree(
+		function (bookmarkTreeNodes) {
+			syncBookmarks(bookmarkTreeNodes);
+		}
+	);
 }
 
+/// MARK: 
 function syncBookmarks(bookmarkNodes) {
 	for(i = 0; i < bookmarkNodes.length; i++) {
 		syncNode(bookmarkNodes[i]);
   	}
 }
 
-function syncNode(node) {
-	if(node.title) {
-		// write title and url of bookmark to firebase
+function pushDataOnFirebase(node) {
+	var database = firebase.database();
 		if(node.parentId) {
-			var nodeRef = database.ref('bookmarks/' + node.parentId);
+			var nodeRef = database.ref('bookmarks/' + node.parentId + "/" + node.id);
 		} else {
-			var nodeRef = database.ref('bookmarks/' + node.id);
+			var nodeRef = database.ref('bookmarks/');
 		}
-
-		// nodeRef.set({	"bookmark_id": node.id,
-  //   					"title": node.title
-  // 					});
-  		
-  		// database.ref().child('bookmarks').set(node.title);
-	}
-
-	// write children bookmark of current bookmark
-	if (node.children && node.children.length > 0) {
-    	syncBookmarks(node.children);
-  	}
+		var nodeInfo = node.url ?  { title: node.title, url: node.url } : { title: node.title };
+		nodeRef.set(nodeInfo);
 }
 
+function syncNode(node) {
+	if(node.title) {
+		// alert("Sync node: " + node.title);
+		pushDataOnFirebase(node)
+	}
+
+	if(node.children == null || node.children.length == 0) {
+		return;
+	}
+
+	var children = node.children;
+	// alert("Begin sync" + children.length + "children node of node: " + node.title);
+	for(i = 0; i < children.length; i++) {
+		syncNode(children[i]);
+	}
+}
+
+/// MARK: Sync button handler
 function syncOnFirebase() {
 	getBookmarkTree();
 }
 
+/// MARK: Setup button click event
 document.addEventListener('DOMContentLoaded', function () {
 	$('#sync_btn').click(function() {
   		syncOnFirebase();
